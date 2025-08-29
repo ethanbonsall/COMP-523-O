ee1886778006c957a475d8d19dfdb400dcd459cd
ethanbonsall <ethanbonsall50@gmail.com>
2025-08-28 23:13:06 -0400

    Added 404 page

---
+++ web/pages/404.tsx
@@ -0,0 +1,10 @@
+const Four04 = () => {
+  return (
+    <div className="min-h-screen bg-background text-center align-middle justify-center gap-8 text-text">
+      <h1>This page is either not set up or doesn&apos;t exist yet.</h1>
+      <h2>Check back later</h2>
+    </div>
+  );
+};
+
+export default Four04;
