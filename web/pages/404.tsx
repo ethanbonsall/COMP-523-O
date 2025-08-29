4f157a38bec46b54ecf7dfbc7bab40a58bc645d8
ethanbonsall <ethanbonsall50@gmail.com>
2025-08-28 23:19:18 -0400

    Small fixes for 404

--- web/pages/404.tsx
+++ web/pages/404.tsx
@@ -1,8 +1,10 @@
 const Four04 = () => {
   return (
-    <div className="min-h-screen bg-background text-center align-middle justify-center gap-8 text-text">
-      <h1>This page is either not set up or doesn&apos;t exist yet.</h1>
-      <h2>Check back later</h2>
+    <div className="min-h-screen flex flex-col bg-background text-center align-middle justify-center gap-8 text-text">
+      <h1 className="text-3xl font-bold">
+        This page is either not set up or doesn&apos;t exist :&#40;
+      </h1>
+      <h2 className="text-2xl font-semibold">Check back later!</h2>
     </div>
   );
 };
